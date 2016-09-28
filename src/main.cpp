9c9202c7c00c509755f9afd327875373837efb60
Antero Nousiainen <antenous@gmail.com>
2016-09-28 19:15:18 +0300

    Create a very inefficient bitmap copier
    Just to try out the Bitmap class the main() is updated to open a bitmap
    file, read it into the Bitmap class and use the class to store a backup
    of the image back to disk. The resulting file should match that of the
    original if all went fine.

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,43 @@
  *      Author: Antero Nousiainen
  */
 
-int main()
+#include <fstream>
+#include <iostream>
+#include "Bitmap.hpp"
+
+using namespace image;
+
+namespace
 {
+    int help( const char * name )
+    {
+        std::cout << "usage: " << name << " <bitmap file>" << std::endl;
+        return 0;
+    }
+}
+
+int main( int argc, char * argv[] )
+{
+    if ( argc != 2 )
+        return help( argv[0] );
+
+    Bitmap bitmap;
+
+    try
+    {
+        std::ifstream bmp( argv[1], std::ios::binary );
+        bitmap.loadFrom( bmp );
+        bmp.close();
+    }
+    catch ( const std::runtime_error & e )
+    {
+        std::cerr << e.what() << std::endl;
+        return 1;
+    }
+
+    std::ofstream bkp( "bkp.bmp", std::ios::binary );
+    bitmap.saveTo( bkp );
+    bkp.close();
+
     return 0;
 }
